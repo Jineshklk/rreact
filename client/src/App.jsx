d23a58a25d465b54eeb3453606efbc627242cd47
Jineshklk <jineshkini2005@gmail.com>
2025-06-26 15:00:08 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Single from "./pages/Single.jsx";
+import Write from "./pages/Write.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
